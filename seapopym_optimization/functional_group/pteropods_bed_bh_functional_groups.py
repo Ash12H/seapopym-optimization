d34d0bc6161a4c05a1e2d013231d05935ebeaf8c
Sarah Albernhe <salbernhe@mercator-ocean.fr>
2025-12-05 10:03:07 +0100

    Cleaning fonction bh

--- seapopym_optimization/functional_group/pteropods_bed_bh_functional_groups.py
+++ seapopym_optimization/functional_group/pteropods_bed_bh_functional_groups.py
@@ -4,22 +4,11 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from seapopym_optimization.functional_group.base_functional_group import AbstractFunctionalGroup, Parameter
+from seapopym_optimization.functional_group import PteropodBedFunctionalGroup, Parameter
 
 
 @dataclass
-class PteropodBedBHFunctionalGroup(AbstractFunctionalGroup):
+class PteropodBedBHFunctionalGroup(PteropodBedFunctionalGroup):
     """The parameters of a functional group as they are defined in the SeapoPym pteropod Bednarsek model, and considering Berverton Holt density dependance."""
 
-    day_layer: float | Parameter
-    night_layer: float | Parameter
-    energy_transfert: float | Parameter
-    lambda_0: float | Parameter
-    gamma_lambda_temperature: float | Parameter
-    gamma_lambda_acidity: float | Parameter
-    survival_rate_0: float | Parameter
-    gamma_survival_rate_temperature: float | Parameter
-    gamma_survival_rate_acidity: float | Parameter
-    tr_0: float | Parameter
-    gamma_tr: float | Parameter
     density_dependance_parameter: float | Parameter
